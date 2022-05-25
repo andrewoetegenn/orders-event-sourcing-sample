5e9e217e25fa9de0846accf5b8e9de280cbb38f1
Andrew Oetegenn <andrewoetegenn@gmail.com>
2022-05-25 22:59:05 +0100

    Bootstrap cdk and configure orders table

--- lib/orders-event-sourcing-sample-stack.ts
+++ lib/orders-event-sourcing-sample-stack.ts
@@ -1,16 +1,13 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { aws_dynamodb, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class OrdersEventSourcingSampleStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'OrdersEventSourcingSampleQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const table = new aws_dynamodb.Table(this, 'OrdersTable', {
+      tableName: 'Orders',
+      partitionKey: { name: 'orderId', type: aws_dynamodb.AttributeType.STRING },
+    });
   }
 }
